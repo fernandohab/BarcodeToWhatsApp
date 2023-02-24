254821ab0d8bbbc527fc452cf28830130a186eb3
Fernando Habitzreiter <85853057+fernandohab@users.noreply.github.com>
2023-02-24 16:08:59 -0300

    Add files via upload

--- UnF_Principal.pas
+++ UnF_Principal.pas
@@ -7,16 +7,18 @@ uses
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
   FMX.Controls.Presentation, FMX.StdCtrls, FMX.Objects, FMX.Media, System.Permissions,
   FMX.MediaLibrary.Actions, System.Actions, FMX.ActnList, FMX.StdActns,
-
-  ZXing.ScanManager,
-  ZXing.ReadResult,
-  ZXing.BarcodeFormat,
-  FMX.Platform,
-  ZXing.ResultPoint,
-
+  // ZXing
+  ZXing.ScanManager, ZXing.ReadResult, ZXing.BarcodeFormat, FMX.Platform, ZXing.ResultPoint,
+  // System
   System.Math.Vectors, FMX.Layouts,  System.Generics.Defaults, System.Generics.Collections,
   System.Diagnostics, System.Threading, System.Math, System.IOUtils, FMX.ListBox, FMX.ExtCtrls,
-  FMX.ScrollBox, FMX.Memo, FMX.Ani, FMX.Effects, System.Rtti, FMX.Memo.Types;
+  FMX.ScrollBox, FMX.Memo, FMX.Ani, FMX.Effects, System.Rtti, FMX.Memo.Types,
+  FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Error, FireDAC.UI.Intf,
+  FireDAC.Phys.Intf, FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Stan.Async,
+  FireDAC.Phys, FireDAC.Phys.SQLite, FireDAC.Phys.SQLiteDef,
+  FireDAC.Stan.ExprFuncs, FireDAC.Phys.SQLiteWrapper.Stat, FireDAC.FMXUI.Wait,
+  FireDAC.Stan.Param, FireDAC.DatS, FireDAC.DApt.Intf, FireDAC.DApt, Data.DB,
+  FireDAC.Comp.DataSet, FireDAC.Comp.Client;
 
 type
   TPrincipal = class(TForm)
@@ -24,6 +26,8 @@ type
     imgCam: TImage;
     CamComponent: TCameraComponent;
     Memo1: TMemo;
+    conn: TFDConnection;
+    qry: TFDQuery;
     procedure FormCreate(Sender: TObject);
     procedure IniciarClick(Sender: TObject);
     procedure CamComponentSampleBufferReady(Sender: TObject; const ATime: TMediaTime);
@@ -84,40 +88,16 @@ var
   ReadResult : TReadResult;
 begin
   CamComponent.SampleBufferToBitmap(imgCam.Bitmap, true);
- { if FScanInProgress then
-    Exit;
-  bmp := TBitmap.Create;
-  bmp.Assign(imgCam.Bitmap);
-  ReadResult := nil;
   try
-    FScanInProgress := true;
-    try
-      ReadResult := FScanManager.Scan(bmp);
-      if ReadResult <> nil then
-      begin
-       //   CamComponent.Active := false;
-          Memo1.Lines.Add(ReadResult.text);
-          close;
-      end;
-    except on E: exception do
-        ShowMessage(E.Message);
-    end;
-  finally
-    bmp.DisposeOf;
-    ReadResult.DisposeOf;
-    FScanInProgress := false;
-    ReadResult := nil;
-  end;  }
-  try
-    // Memo1.Lines.Add('Here');
+    // Memo1.Lines.Add('Init');
     bmp := TBitmap.Create;
     bmp.Assign(imgCam.Bitmap);
     ReadResult := FScanManager.Scan(bmp);
     if ReadResult <> nil then
       begin
-       //   CamComponent.Active := false;
+       // CamComponent.Active := false;
           Memo1.Lines.Add(ReadResult.text);
-          close;
+          Close;
       end;
   finally
     bmp.Free;
